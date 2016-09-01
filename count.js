bfb55fed3bce1ef1cdfcb75bade6595fa7c30db0
Christian Prajzner <christian@Christians-MBP.home>
2016-09-01 13:50:39 -0400

    finshed the function and it works correctly, throwing an error for invalid input

--- count.js
+++ count.js
@@ -5,24 +5,30 @@ function count(input){
   }
   onesNzerosArray = input.split("");
 
-  if (onesNzerosArray.lenght != 10){
+  if (onesNzerosArray.length != 10){
     throw "invalid argument provide a 10 digit string"
   }
 
   let result = 0;
   for (let i = 0; i < onesNzerosArray.length; i++){
-    if(onesNzerosArray[i] != 1 || onesNzerosArray[i] != 0){
+    if(onesNzerosArray[i] != 1 && onesNzerosArray[i] != 0){
       throw "Invalid arguemnt, all digits of input must get 1s or 0s";
     }
-    if(i == 4){
+    if(i ==4 && onesNzerosArray[i] == 1){
       result += 50;
     }
-    if(i ==5 ){
+    if(i ==5 && onesNzerosArray[i] == 1){
       result += 5;
+    }
+    if(i ==3 && onesNzerosArray[i] == 1){
+      result += 10;
+    }
+    if(i ==6 && onesNzerosArray[i] == 1){
+      result += 1;
     }
   }
 
-  //done error checking.. :)
+
   /*
   0 - left pinky
   1 - left ring
@@ -36,5 +42,29 @@ function count(input){
   9 - right pinky
   */
 
-  if()
+
+  for (let left = 2,  right = 7; right < 10; right ++, left --){
+    if(onesNzerosArray[left] ==1 ){
+      if(onesNzerosArray[left + 1 ] == 0 ){
+        throw "invalid number";
+      }
+      else {
+        result += 10;
+      }
+    }
+    if(onesNzerosArray[right] ==1){
+      if(onesNzerosArray[right - 1 ] ==0){
+        throw "invalid number";
+      }
+      else {
+        result += 1;
+      }
+    }
+
+  }
+
+  return result;
+
 }
+
+console.log(count("0000110000"));
